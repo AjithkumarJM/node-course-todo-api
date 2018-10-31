d51545b6b61c0a9b8c022ea22ca75c5857ddbb4b
AjithkumarJM <ajithkumarjm@gmail.com>
2018-11-01 01:03:04 +0530

    changes after setting up auth token in TODO's

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -44,8 +45,7 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-  // user.tokens.push({access, token});
-
+  // user.tokens.push({ access, token });
   user.tokens = user.tokens.concat([{ access, token }])
 
   return user.save().then(() => {
@@ -53,9 +53,75 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  // to find the appropirate user
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = { User }
+
 
 
 // var user = new User({
--- server/server.js
+++ server/server.js
@@ -3,149 +3,149 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ObjectId } = require('mongodb');
+const {ObjectID} = require('mongodb');
 
-const { mongoose } = require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
-const port = process.env.PORT || 3000;
 var app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
+  var todo = new Todo({
+    text: req.body.text,
+    _creator: req.user._id
+  });
 
-    var todo = new Todo({
-        text: req.body.text
-    })
-
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (error) => {
-        res.status(400).send(error);
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({
-            todos,
-            status: 200
-        })
-    }, (e) => {
-        if (e) {
-            res.status(400).send(e)
-        }
-    })
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    // res.send(req.params);
-    if (!ObjectId.isValid(id)) {
-        return res.status(404).send({
-            message: 'Id is invalid'
-        });
+app.get('/todos/:id', authenticate, (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send({
-                message: `no todo find on this id ${id}`,
-            });
-        }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
-        res.send({
-            todo,
-            status: 200
-        })
-    }).catch((e) => {
-        if (e) {
-            res.status(400).send({
-                message: `no todo find on this id ${id}`,
-            });
-        }
-    })
-})
+app.delete('/todos/:id', authenticate, (req, res) => {
+  var id = req.params.id;
 
-app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-    if (!ObjectId.isValid(id)) {
-        return res.status(404).send({
-            message: "Invalid Id",
-        })
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send({
-                message: "no id provided"
-            })
-        }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
-        res.send({ todo });
-    }).catch((e) => {
-        res.status(400).send({
-            message: e
-        })
-    })
+app.patch('/todos/:id', authenticate, (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
 
-})
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
 
-app.patch('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    var body = _.pick(req.body, ['text', 'completed']);
-
-    if (!ObjectId.isValid(id)) {
-        return res.status(404).send({
-            message: "Invalid Id",
-        })
+  Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
-    } else {
-        body.completed = false;
-        body.completedAt = null
-    }
-
-    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
-        if (!todo) {
-            return res.status(404).send({
-                message: "no id provided"
-            })
-        }
-
-        res.send({ todo });
-    }).catch((e) => {
-        res.status(400).send({
-            message: e
-        })
-    })
-})
-
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 // POST /users
 app.post('/users', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    var user = new User(body);
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
-        res.header('x-auth', token).send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-    console.log(`server started at ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
-module.exports = {
-    app
-}
+module.exports = {app};
